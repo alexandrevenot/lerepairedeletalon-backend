e7075d90dbee09388a02d037ce76a367b6715257
Alexandre <venotalexandre@gmail.com>
2023-07-16 22:17:16 +0200

    Updated geolocation to make it more user friendly

--- current/bin/src/api/geoloc/router.py
+++ current/bin/src/api/geoloc/router.py
@@ -10,58 +10,42 @@ import src.api.geoloc.utils as utils
 router = APIRouter(prefix='/geoloc')
 
 @router.get('/get-city', response_model=schemas.GetCitiesRM)
-async def get_city(user_input: str):
-    # parse user_input
-    user_input = user_input.replace("(", "")
-    user_input = user_input.replace(")", "")
+async def get_city(city: str, requested_postal_code: str = None):
 
-    matches = re.findall(r'\d+', user_input) # get chains of numeric characters
-    input_includes_a_postal_code = False
-    requested_postal_code = None
-    if len(matches) == 1:
-        requested_postal_code = matches[0]
-        input_includes_a_postal_code = True
-    elif len(matches) > 1:
-        raise HTTPException(status_code=404, detail=f"city not found")
+    request_includes_a_postal_code = requested_postal_code is not None
 
     # match user_input
     try:
-        if input_includes_a_postal_code:
-            user_input = user_input.replace(requested_postal_code, "")
-
-        result = utils.find_city(user_input)
+        result = utils.find_city(city)
     except:
         print(traceback.format_exc())
         raise HTTPException(status_code=500, detail="failed to browse postal codes db")
 
-    if result:
-        cities_to_return = []
-        for city in result:
-            if input_includes_a_postal_code:
-                postal_code=str(city['postal_code'])
-                if requested_postal_code in postal_code:
-                    cities_to_return.append(schemas.CityRM(
-                        city_name=city["city"],
-                        postal_code=postal_code,
-                        lat=city['lat'],
-                        lng=city['long']
-                    ))
-            else:
-                cities_to_return.append(schemas.CityRM(
-                city_name=city["city"],
-                postal_code=str(city['postal_code']),
-                lat=city['lat'],
-                lng=city['long']
-                ))
-        
-        if len(cities_to_return) == 0:
-            raise HTTPException(status_code=404, detail=f"city not found")
-        elif len(cities_to_return) > 10:
-            raise HTTPException(status_code=422, detail={
-                "message": "too many results",
-                "value": len(cities_to_return)
-            })
-        else:
-            return schemas.GetCitiesRM(content=cities_to_return)
-    else:
+    if not result:
         raise HTTPException(status_code=404, detail=f"city not found")
+
+    cities_to_return = []
+    for city in result:
+        if request_includes_a_postal_code:
+            postal_code=str(city['postal_code'])
+            if requested_postal_code in postal_code:
+                cities_to_return.append(schemas.CityRM(
+                    city_name=city["city"],
+                    postal_code=postal_code,
+                    lat=city['lat'],
+                    lng=city['long']
+                ))
+        else:
+            cities_to_return.append(schemas.CityRM(
+            city_name=city["city"],
+            postal_code=str(city['postal_code']),
+            lat=city['lat'],
+            lng=city['long']
+            ))
+
+    if len(cities_to_return) == 0:
+        raise HTTPException(status_code=404, detail=f"city not found")
+    elif len(cities_to_return) > 10:
+        index_min = min(range(len([elt.postal_code for elt in cities_to_return])), key=[elt.postal_code for elt in cities_to_return].__getitem__)
+        cities_to_return = [cities_to_return[index_min]]
+    return schemas.GetCitiesRM(content=cities_to_return)
