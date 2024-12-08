4b1c9140dca8421c5b49d6c05d77f13d6e4f5dd3
Alexandre Venot <venotalexandre@gmail.com>
2024-12-09 00:18:02 +0100

    Updated build.sh

--- build.sh
+++ build.sh
@@ -6,8 +6,10 @@ cp app/dependencies.py app_build/
 cp app/middleware.py app_build/
 cp app/requirements.txt app_build/
 cp app/prestart.sh app_build/
-cp -r app/routers app_build/
-cp -r app/etc app_build/
+cp -r app/routers/ app_build/
+cp -r app/etc/ app_build/
+cp -r app/monitoring/ app_build/
+find app_build/ -type d -name '__pycache__' -exec rm -r {} +
 mkdir app_build/logs/
 
 VERSION=$(cat VERSION | tr -d '\n')
