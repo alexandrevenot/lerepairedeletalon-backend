db9b8388fe947c91905b7b039820125c90a8dfd6
Alexandre Venot <venotalexandre@gmail.com>
2026-02-17 14:00:42 +0000

    Deploy test version

--- app/prestart.sh
+++ app/prestart.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-export GOOGLE_APPLICATION_CREDENTIALS="/app/etc/gcloud/sas-lrde-0-85c8175024af.json"
+export GOOGLE_APPLICATION_CREDENTIALS="/app/etc/gcloud/brilliant-tower-404410-7fc9dd462a4b.json"
